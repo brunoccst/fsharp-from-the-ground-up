aae4c9faef6692aebbf390dd30d3e47800814d0d
Bruno Carvalho da Costa <Bruno.DaCosta@docuware.com>
2021-10-04 17:05:02 +0200

    Changed program to use arguments

--- Program.fs
+++ Program.fs
@@ -7,7 +7,7 @@ let from whom =
     sprintf "from %s" whom
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+let main argv = // string[] -> int
+    let person = argv.[0]
+    printfn "Hello %s from my F# program!" person
     0 // return an integer exit code
